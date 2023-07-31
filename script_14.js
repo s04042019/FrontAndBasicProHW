b47a74e607fbabbcd7a5764d124c40bead77afef
s04042019 <97693499+s04042019@users.noreply.github.com>
2023-07-31 12:26:18 +0300

    script_14

---
+++ script_14.js
@@ -0,0 +1,14 @@
+const number = +prompt('Введіть число між 0 та 3', '');
+
+switch (number) {
+    case 0:
+        alert('Ви ввели число 0'); 
+        break;
+    case 1:
+        alert('Ви ввели число 1'); 
+        break;
+    case 2:
+    case 3:    
+        alert('Ви ввели число 2, а може й 3'); 
+        break;           
+}
